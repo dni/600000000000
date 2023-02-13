4e64fc1559e0e801e78d2aec6b4fa5f24c7608aa
dni ⚡ <office@dnilabs.com>
2023-02-13 22:06:38 +0100

    fix inscribe batch script

--- inscriptions/inscribe.sh
+++ inscriptions/inscribe.sh
@@ -2,7 +2,8 @@
 ord_bin=./ord
 btc_datadir=/Bitcoin/
 datadir=/ord/
+repeat() { while :; do $@ && return; sleep 30; done }
 while IFS= read -r line; do
   echo "inscribing 600-$line.svg..."
-  echo "$ord_bin --bitcoin-data-dir $btc_datadir --data-dir $datadir wallet inscribe imgs/600-$line.svg --fee-rate 4"
+  repeat echo "$ord_bin --bitcoin-data-dir $btc_datadir --data-dir $datadir wallet inscribe imgs/600-$line.svg --fee-rate 4"
 done < ord-colors.txt
